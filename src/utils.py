f87768ce1b9898d6f7513f6112c7652028d6538b
Geeta Keote <geetaKeote@gmail.com>
2023-06-10 15:18:59 +0530

    prediction pipeline complted

--- src/utils.py
+++ src/utils.py
@@ -47,5 +47,7 @@ def evaluate_model(X_train, y_train, X_test, y_test, models, params):
 
             return report
 
+            
+
     except Exception as e:
         raise CustmeException(e, sys)
